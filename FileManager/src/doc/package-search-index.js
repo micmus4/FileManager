2f806d02726a86af4ed06b121eeef81d68afba06
mmusialowicz <71464021+micmus4@users.noreply.github.com>
2021-05-25 02:31:44 +0200

    Delete FileManager/src/doc directory

--- FileManager/src/doc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"m":"FileManager","l":"pl.musialowicz.filemanager"}]
--- FileManager/src/doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"pl.musialowicz.filemanager","l":"Controller"},{"p":"pl.musialowicz.filemanager","l":"CreateDirectory"},{"p":"pl.musialowicz.filemanager","l":"Main"}]
